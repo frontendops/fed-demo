3f473b5b0b7a9ce78fb1e47dd092cbef5cf3d665
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-06-20 23:19:22 -0400

    emit fix

---
+++ src/components/Gallery.vue
@@ -0,0 +1,46 @@
+<template>
+  <div role="list" class="gallery">
+    <div
+      class="gallery-item"
+      role="list-item"
+      v-for="item in items"
+      :style="`background: ${item.color}`"
+      :key="item.name"
+    >
+      <div>{{item.name}}</div>
+      <div>${{item.price}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gallery",
+  props: {
+    items: Array
+  }
+};
+</script>
+
+<style scoped>
+.gallery {
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 16px;
+  border: 1px solid black;
+  border-radius: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  font-size: 2rem;
+  font-weight: bold;
+  color: white;
+}
+
+.gallery-item {
+  height: 250px;
+  width: 250px;
+  padding: 8px;
+  margin-right: 16px;
+}
+</style>
---
+++ src/components/ScopedList.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- make this a scoped slot -->
+  <div>
+    <h1>{{ title }}</h1>
+    <div class="total-price">
+      <span>Your Items</span>
+    </div>
+    <!-- make this a slot and move this code up to the parent 🤗 -->
+    <div role="list" class="scoped-list">
+      <slot v-bind:items="subscriptions"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScopedList",
+  props: {
+    ListItems: Array
+  },
+  data() {
+    return {
+      title: "Your subscriptions",
+      subscriptions: [
+        {
+          name: "Netflix",
+          price: 10,
+          color: "#e31929"
+        },
+        {
+          name: "Spotify",
+          price: 10,
+          color: "#028858"
+        },
+        {
+          name: "Medium",
+          price: 5,
+          color: "#024c8e"
+        },
+        {
+          name: "Headspace",
+          price: 12,
+          color: "#f9af2f"
+        },
+        {
+          name: "Amazon Prime",
+          price: 12,
+          color: "#f9af2f"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="css">
+.scoped-list {
+  margin: 0 auto;
+  margin-top: 16px;
+  max-width: 900px;
+  padding: 0 8px;
+}
+
+.total-price {
+  display: flex;
+  font-size: 2rem;
+  max-width: 900px;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 0 8px;
+}
+
+.total-price__total {
+  font-weight: bold;
+}
+
+.lines {
+  border-bottom: 2px dotted black;
+  flex-grow: 1;
+}
+</style>
+
+
